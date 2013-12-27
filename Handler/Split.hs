a96d751565b9aa4322a65201b22c1996c30f9abc
Eric Rochester <erochest@virginia.edu>
2013-12-27 15:47:37 -0500

    Split form page.

--- Handler/Split.hs
+++ Handler/Split.hs
@@ -3,26 +3,54 @@
 module Handler.Split where
 
 
-import           Control.Error
-import qualified Data.Text     as T
+import qualified Data.Text           as T
 import           Import
 import           Language.Haskell.TH (Exp(..))
 
+import           Text.XML.Split
+import qualified Text.XML.Split      as S
+
+
+data Hole = Hole
 
 getSplitR :: Handler Html
 getSplitR = do
-    docs <- fmap (fmap redoc) . runDB $ selectList [] [Asc DocumentTitle]
+    docs <- documentOptions
+    let formWidget = splitFormWidget docs
     defaultLayout $ do
         $(fayFile' (ConE 'StaticR) "Split")
-        $(widgetFile "split_form")
-    where redoc e = ( either (("ERROR: " <>) . T.pack) id
-                    . fromPersistValueText . unKey
-                    $ entityKey e
-                    , entityVal e
-                    )
+        $(widgetFile "split")
 
 postSplitR :: Handler Html
 postSplitR = undefined
 
 getSplitDownloadR :: Handler TypedContent
 getSplitDownloadR = undefined
+
+data SplitInfo = SI
+               { splitDocuments   :: [Entity Document]
+               , splitDivision    :: Division
+               , splitChunkSize   :: Int
+               , splitChunkOffset :: Int
+               }
+
+splitFormWidget :: OptionList (Entity Document) -> Widget
+splitFormWidget docs = do
+    $(whamletFile "templates/split_form.hamlet")
+
+documentOptions :: Handler (OptionList (Entity Document))
+documentOptions = optionsPersist [] [Asc DocumentTitle] documentTitle
+
+divisionOptions :: [(T.Text, Division)]
+divisionOptions = [ ("document", S.Document)
+                  , ("section",  Section)
+                  , ("page",     Page)
+                  , ("speaking", Speaking)
+                  ]
+
+splitForm :: FormInput Handler SplitInfo
+splitForm = SI <$> ireq (multiSelectField documentOptions) "document"
+               <*> ireq (radioFieldList divisionOptions)   "division"
+               <*> ireq intField                           "chunksize"
+               <*> ireq intField                           "chunkoffset"
+
