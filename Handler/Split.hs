3ed532fbcee820d68a7d148adc6cdb3efbaaeb56
Eric Rochester <erochest@virginia.edu>
2013-12-29 16:04:16 -0500

    The split form destination page.
    This page just has a finished message and triggers the actual download
    through JS.

--- Handler/Split.hs
+++ Handler/Split.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 
 module Handler.Split where
 
 
+import           Data.Maybe
+import           Data.Monoid
 import qualified Data.Text           as T
+import           Database.Persist
 import           Import
 import           Language.Haskell.TH (Exp(..))
 
@@ -22,7 +27,10 @@ getSplitR = do
         $(widgetFile "split")
 
 postSplitR :: Handler Html
-postSplitR = undefined
+postSplitR = do
+    urlParams <- splitUrlParams <$> runInputPost splitForm
+    defaultLayout $ do
+        $(widgetFile "split_done")
 
 getSplitDownloadR :: Handler TypedContent
 getSplitDownloadR = undefined
@@ -34,6 +42,25 @@ data SplitInfo = SI
                , splitChunkOffset :: Int
                }
 
+splitUrlParams :: SplitInfo -> T.Text
+splitUrlParams SI{..} =
+    mconcat [ "chunksize=",   showt splitChunkSize
+            , "&chunkoffset=", showt splitChunkOffset
+            , maybe "" (mappend "&division=" . showt)
+            $ findi ((== splitDivision) . snd) 1 divisionOptions
+            , docParams
+            ]
+    where docParams   = foldr docp mempty $ mapMaybe intKey splitDocuments
+          docp p rest = "&document=" <> showt p <> rest
+          showt       = T.pack . show
+          intKey (Entity k _) = case unKey k of
+                                    PersistInt64 int -> Just int
+                                    _                -> Nothing
+          findi :: (a -> Bool) -> Int -> [a] -> Maybe Int
+          findi _ _ []                 = Nothing
+          findi p i (x:xs) | p x       = Just i
+                           | otherwise = findi p (i + 1) xs
+
 splitFormWidget :: OptionList (Entity Document) -> Widget
 splitFormWidget docs = do
     $(whamletFile "templates/split_form.hamlet")
@@ -42,10 +69,10 @@ documentOptions :: Handler (OptionList (Entity Document))
 documentOptions = optionsPersist [] [Asc DocumentTitle] documentTitle
 
 divisionOptions :: [(T.Text, Division)]
-divisionOptions = [ ("document", S.Document)
-                  , ("section",  Section)
-                  , ("page",     Page)
-                  , ("speaking", Speaking)
+divisionOptions = [ ("Document", S.Document)
+                  , ("Section",  Section)
+                  , ("Page",     Page)
+                  , ("Speaking", Speaking)
                   ]
 
 splitForm :: FormInput Handler SplitInfo
