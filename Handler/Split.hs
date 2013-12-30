709fd9df05bb9f5536eae6b1f316f2ce84efc74d
Eric Rochester <erochest@virginia.edu>
2013-12-30 10:58:00 -0500

    Added download link.

--- Handler/Split.hs
+++ Handler/Split.hs
@@ -5,15 +5,23 @@
 module Handler.Split where
 
 
+import           Codec.Archive.Zip
+import           Control.Monad
+import           Data.ByteString.Lazy      (fromStrict)
+import qualified Data.Char                 as C
+import           Data.List                 (foldl')
 import           Data.Maybe
 import           Data.Monoid
-import qualified Data.Text           as T
+import qualified Data.Text                 as T
+import qualified Data.Text.Encoding        as E
+import           Data.Time.Clock.POSIX
 import           Database.Persist
 import           Import
-import           Language.Haskell.TH (Exp(..))
+import           Language.Haskell.TH       (Exp(..))
+import qualified Filesystem.Path.CurrentOS as FS
 
 import           Text.XML.Split
-import qualified Text.XML.Split      as S
+import qualified Text.XML.Split            as S
 
 
 data Hole = Hole
@@ -33,7 +41,12 @@ postSplitR = do
         $(widgetFile "split_done")
 
 getSplitDownloadR :: Handler TypedContent
-getSplitDownloadR = undefined
+getSplitDownloadR = do
+    sp     <- runInputGet splitForm
+    splits <- liftIO $ split sp
+    let base = T.append "by-" . T.toLower . T.pack . show $ splitDivision sp
+    addHeader "content-disposition" $ "attachment; filename=" <> base <> ".zip"
+    liftIO (archiveSplits base splits) >>= return . TypedContent "application/zip" . toContent . fromArchive
 
 data SplitInfo = SI
                { splitDocuments   :: [Entity Document]
@@ -41,6 +54,69 @@ data SplitInfo = SI
                , splitChunkSize   :: Int
                , splitChunkOffset :: Int
                }
+
+split :: SplitInfo -> IO [Split]
+split SI{..} = concat <$> mapM (split' . entityVal) splitDocuments
+    where split' :: Import.Document -> IO [Split]
+          split' Import.Document{..} =
+            S.splitDocument splitDivision
+                            (splitChunkSize, splitChunkOffset)
+                            (FS.fromText documentSourceFile)
+                            documentContent
+
+archiveSplits :: T.Text -> [Split] -> IO Archive
+archiveSplits dir ss = do
+    now <- truncate <$> getPOSIXTime
+    return . foldl' (flip addEntryToArchive) emptyArchive
+           $ map (splitEntry now) ss
+    where splitEntry now s@Split{..} = toEntry (makeArchivePath dir' s) now
+                                    . fromStrict
+                                    $ E.encodeUtf8 _splitText
+          dir' = FS.fromText dir
+
+makeArchivePath :: FS.FilePath -> Split -> FilePath
+makeArchivePath dir Split{..} =
+    FS.encodeString $ dir FS.</> (FS.fromText $ T.intercalate "-" parts)
+                          FS.<.> "txt"
+    where parts = [ toText' _splitPath
+                  , clean _splitId
+                  , T.pack (show _splitN)
+                  ]
+          toText' = either id id . FS.toText
+
+clean :: T.Text -> T.Text
+clean = T.filter (\c -> C.isAscii c && C.isAlphaNum c) . T.map char
+
+char :: Char -> Char
+char '\x03b1' = 'a'
+char '\x03b2' = 'b'
+char '\x03b3' = 'g'
+char '\x03b4' = 'd'
+char '\x03b5' = 'e'
+char '\x03b6' = 'z'
+char '\x03b7' = 'h'
+char '\x03b8' = 'q'
+char '\x03b9' = 'i'
+char '\x03ba' = 'k'
+char '\x03bb' = 'l'
+char '\x03bc' = 'm'
+char '\x03bd' = 'n'
+char '\x03be' = 'c'
+char '\x03bf' = 'o'
+char '\x03c0' = 'p'
+char '\x03c1' = 'r'
+char '\x03c2' = 's'
+char '\x03c3' = 's'
+char '\x03c4' = 't'
+char '\x03c5' = 'u'
+char '\x03c6' = 'f'
+char '\x03c7' = 'x'
+char '\x03c8' = 'y'
+char '\x03c9' = 'w'
+char '\x03dd' = 'v'
+char '\x03f2' = 's'
+char c        = c
+
 
 splitUrlParams :: SplitInfo -> T.Text
 splitUrlParams SI{..} =
