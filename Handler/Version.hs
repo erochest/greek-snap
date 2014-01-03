236c9c26f4423a63f4a4f99afb18f623f9e4cfed
Eric Rochester <erochest@virginia.edu>
2014-01-03 10:39:07 -0500

    Added JSON version REST endpoint.

--- Handler/Version.hs
+++ Handler/Version.hs
@@ -4,15 +4,32 @@
 module Handler.Version where
 
 
+import           Control.Error
+import qualified Data.Text as T
 import           Data.Version
 import           Import
 
 import           Paths_greek_snap (version)
 
 
+getVersionLevel :: Int -> Version -> Int
+getVersionLevel n = either id id . (flip (atErr 0) n) . versionBranch
+
+getVersionTags :: Version -> Maybe T.Text
+getVersionTags = go . versionTags
+    where go [] = Nothing
+          go xs = Just . T.intercalate "." $ map T.pack xs
+
 getVersionR :: Handler TypedContent
 getVersionR = selectRep $ do
     provideRep $ defaultLayout $ do
         $(widgetFile "version")
+    provideRep $ return $ object
+        [ "major" .= getVersionLevel 0 version
+        , "minor" .= getVersionLevel 1 version
+        , "patch" .= getVersionLevel 2 version
+        -- , "build" .= getVersionLevel 3 version
+        , "tags"  .= getVersionTags version
+        ]
 
 
