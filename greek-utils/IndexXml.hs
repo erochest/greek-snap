b6613ec02f8e41543ef3824e79520816e49a6c45
Eric Rochester <erochest@virginia.edu>
2014-02-06 12:10:51 -0500

    List hit contexts with lines read from file.

--- greek-utils/IndexXml.hs
+++ greek-utils/IndexXml.hs
@@ -1,12 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE TemplateHaskell   #-}
 {-# LANGUAGE TupleSections     #-}
 {-# OPTIONS_GHC -Wall #-}
 
 -- TODOS:
--- [ ] list hit contexts, grouped by files and ordered by line
 -- [ ] combine hit contexts
--- [ ] get contexts from files and print them
 -- [ ] highlighting
 -- [ ] highlighting options
 
@@ -16,6 +15,7 @@ module Main where
 
 import           Control.Applicative
 import           Control.Arrow
+import           Control.Lens
 import           Control.Monad
 import qualified Data.Char                 as C
 import           Data.Conduit
@@ -29,11 +29,15 @@ import           Data.Maybe
 import           Data.Monoid
 import qualified Data.Text                 as T
 import           Data.Text.ICU
+import qualified Data.Text.IO              as TIO
+import qualified Data.Text.Lazy            as TL
+import qualified Data.Text.Lazy.Builder    as TB
 import           Data.XML.Types
 import           Filesystem
 import           Filesystem.Path.CurrentOS hiding (filename)
 import qualified Filesystem.Path.CurrentOS as FS
-import           Options.Applicative
+import           Options.Applicative       hiding ((&))
+import qualified Options.Applicative       as O
 import           Prelude                   hiding (FilePath, break, lines)
 import           Text.XML.Stream.Parse
 
@@ -45,11 +49,20 @@ import           Debug.Trace
 type FileLocation  = (FilePath, PositionRange)
 type InvertedIndex = M.HashMap T.Text (D.DList FileLocation)
 
+data HitContext = HitContext
+                { _contextLocation :: FileLocation
+                , _contextTerm     :: T.Text
+                , _contextCount    :: Int
+                , _contextLines    :: [T.Text]
+                } deriving (Show)
+makeLenses ''HitContext
+
+
 grc :: LocaleName
 grc = Locale "grc"
 
 indexFile :: InvertedIndex -> FilePath -> IO InvertedIndex
-indexFile index xmlFile = runResourceT $
+indexFile iindex xmlFile = runResourceT $
        CB.sourceFile (FS.encodeString xmlFile)
     $= parseBytesPos def
     $= CL.filter (isContentEvent . snd)
@@ -57,7 +70,7 @@ indexFile index xmlFile = runResourceT $
     $= CL.map (fmap getText)
     $= CL.concatMap (spread tokenizeText)
     $= CL.map offsetWord
-    $$ CL.fold (indexTokens xmlFile) index
+    $$ CL.fold (indexTokens xmlFile) iindex
 
 isContentEvent :: Event -> Bool
 isContentEvent (EventContent _) = True
@@ -96,22 +109,70 @@ offsetWord (mpos, break) = (offset break pos, brkBreak break)
 
 indexTokens :: FilePath -> InvertedIndex -> (PositionRange, T.Text)
             -> InvertedIndex
-indexTokens filename index (pos, token) =
-    M.insertWith mappend token (D.singleton (filename, pos)) index
+indexTokens filename iindex (pos, token) =
+    M.insertWith mappend token (D.singleton (filename, pos)) iindex
 
+hitContext :: T.Text -> Int -> FileLocation -> [T.Text] -> HitContext
+hitContext term context fileLoc =
+    HitContext fileLoc term context . extractLines (snd fileLoc) context
+
+extractLines :: PositionRange -> Int -> [a] -> [a]
+extractLines (PositionRange start end) context =
+    L.take lineRange . L.drop startLine
+    where startLine = posLine start - context
+          endLine   = posLine end   + context
+          lineRange = max 1 $ endLine - startLine
+
+formatContext :: HitContext -> T.Text
+formatContext HitContext{..} = TL.toStrict . TB.toLazyText . mconcat $
+    [ TB.fromText _contextTerm
+    , colon
+    , TB.fromString (encodeString filepath)
+    , colon
+    , TB.fromString (show prange)
+    , nl
+    ]
+    ++ map ((<> nl) . TB.fromText) _contextLines
+    where (filepath, prange) = _contextLocation
+          nl    = TB.singleton '\n'
+          colon = TB.singleton ':'
+
+onFirsts :: Eq a => (a, b) -> (a, c) -> Bool
+onFirsts = curry (uncurry (==) . (fst *** fst))
+
+putnl :: IO ()
+putnl = putStrLn ""
+
+underscore :: Char -> T.Text -> IO ()
+underscore c t =  TIO.putStrLn t
+               >> TIO.putStrLn (T.replicate (T.length t) $ T.singleton c)
+               >> putnl
+
+printFileGroup :: Int -> T.Text -> [FileLocation] -> IO ()
+printFileGroup context query hits@((filepath, _):_) = do
+    lines <- T.lines <$> TIO.readFile (encodeString filepath)
+    underscore '-' . T.pack $ encodeString filepath
+    mapM_ (TIO.putStrLn . formatContext . flip (hitContext query context) lines)
+          hits
+
+printFileGroup _     _       []          = return ()
 
 main :: IO ()
 main = do
     IX{..} <- execParser indexXmlOpts
-    index  <- foldM indexFile M.empty =<< listDirectory xmlDir
-    let hits = L.sort . maybe [] D.toList $ M.lookup queryTerm index
-    forM_ hits $ \(filename, prange) ->
-        putStrLn $ encodeString filename ++ ":" ++ show prange
+    underscore '=' queryTerm
+    mapM_ (printFileGroup contextN queryTerm)
+        . L.groupBy onFirsts
+        . L.sort
+        . maybe [] D.toList
+        . M.lookup queryTerm
+        =<< foldM indexFile M.empty
+        =<< listDirectory xmlDir
 
 data IndexXml = IX
-              { xmlDir       :: FilePath
-              , contextLines :: Int
-              , queryTerm    :: T.Text
+              { xmlDir    :: FilePath
+              , contextN  :: Int
+              , queryTerm :: T.Text
               } deriving (Show)
 
 indexXmlOpts :: ParserInfo IndexXml
@@ -130,6 +191,6 @@ indexXml =   IX
                     <> value 2
                     <> help "The number of context lines before and after\
                             \ each hit. (default = 2)")
-        <*> argument (Just . T.pack)
-                     (metavar "QUERY" <> help "The query to search for.")
+        <*> O.argument (Just . T.pack)
+                       (metavar "QUERY" <> help "The query to search for.")
     where xmlDir     = "../gk-texts"
