071c0de2716d5d2183e961df775c24436182149b
Eric Rochester <erochest@virginia.edu>
2014-02-07 11:21:21 -0500

    Stub out count-tokens.

---
+++ greek-utils/CountTokens.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+
+module Main where
+
+
+import qualified Data.HashMap.Strict as M
+import qualified Data.Text           as T
+import qualified Data.Text.IO        as TIO
+import           Filesystem
+import           Options.Applicative
+import           Prelude             hiding (FilePath, break, lines)
+
+import           IndexXml.Index
+import           IndexXml.Types
+
+
+main :: IO ()
+main = undefined
+
