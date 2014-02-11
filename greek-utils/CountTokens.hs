02c2948f79351118030ce77778b23e1a079be380
Eric Rochester <erochest@virginia.edu>
2014-02-11 16:23:50 -0500

    Added a utility to count tokens.

--- greek-utils/CountTokens.hs
+++ greek-utils/CountTokens.hs
@@ -1,20 +1,55 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
 
 
 module Main where
 
 
-import qualified Data.HashMap.Strict as M
-import qualified Data.Text           as T
-import qualified Data.Text.IO        as TIO
+import           Control.Monad
+import qualified Data.DList                as D
+import qualified Data.HashMap.Strict       as M
+import qualified Data.List                 as L
+import           Data.Ord
+import qualified Data.Text                 as T
+import qualified Data.Text.IO              as TIO
 import           Filesystem
+import           Filesystem.Path.CurrentOS
 import           Options.Applicative
-import           Prelude             hiding (FilePath, break, lines)
+import           Prelude                   hiding (FilePath, break, lines)
 
 import           IndexXml.Index
 import           IndexXml.Types
+import           Utils
 
+
+formatPair :: (T.Text, Int) -> T.Text
+formatPair (t, i) = t <> "\t" <> T.pack (show i)
+
+countHits :: D.DList a -> Int
+countHits = length . D.toList
+
+sortFreqs :: [(T.Text, Int)] -> [(T.Text, Int)]
+sortFreqs = L.sortBy (flip (comparing snd))
+
+indexFreqs :: InvertedIndex -> [(T.Text, Int)]
+indexFreqs = sortFreqs . map (fmap countHits) . M.toList
 
 main :: IO ()
-main = undefined
+main =   Main.xmlDir <$> execParser countTokensOpts
+     >>= listDirectory
+     >>= fmap indexFreqs . foldM indexFile M.empty
+     >>= mapM_ (TIO.putStrLn . formatPair)
 
+data CountTokens = CT
+                 { xmlDir :: FilePath
+                 } deriving (Show)
+
+countTokensOpts :: ParserInfo CountTokens
+countTokensOpts = info (helper <*> countTokens)
+                       (  fullDesc
+                       <> progDesc "Dump the token frequencies as TSV."
+                       <> header "count-tokens: Dumps token frequencies."
+                       )
+
+countTokens :: Parser CountTokens
+countTokens = CT <$> xmlDirOption "../gk-texts"
